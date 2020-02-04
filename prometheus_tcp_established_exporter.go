9919f2652d874865a4162f9efed59073c8b8b3f7
culpinnis <culpinnis@gmail.com>
2020-02-04 20:38:09 +0100

    Renamed metric according to best practise. Added labels (not finished)

--- prometheus_tcp_established_exporter.go
+++ prometheus_tcp_established_exporter.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/namsral/flag"
 	"fmt"
+	"strings"
 	"github.com/cakturk/go-netstat/netstat"
 	"net"
 	"time"
@@ -15,37 +16,21 @@ import (
 
 const(
 	proto = 0x01 | 0x02
-	myport = 2112
 )
 var(
 	tcpv6 bool = true
 	port int = -1
+	myport int = 9669
+	duration int = 6
 )
 var(
 	netstat_tcp_connection_longterm_counts = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "netstat_tcp_connection_longterm_counts",
+		Name: "netstat_tcp_longterm_connections_total",
 		Help: "TCP connections that are established for a minimal duration"})
 )
 func init(){
 	prometheus.MustRegister(netstat_tcp_connection_longterm_counts)
 }
-func displaySockInfo(s []netstat.SockTabEntry) {
-	lookup := func(skaddr *netstat.SockAddr) string {
-		addr := skaddr.IP.String()
-			names, err := net.LookupAddr(addr)
-			if err == nil && len(names) > 0 {
-				addr = names[0]
-			}
-		return fmt.Sprintf("%s %d", addr, skaddr.Port)
-	}
-
-	for _, e := range s {
-		saddr := lookup(e.LocalAddr)
-		daddr := lookup(e.RemoteAddr)
-		fmt.Printf("%s %s %s \n", saddr, daddr, e.State)
-	}
-}
-
 func countSockInfo(connection_counts map[string]uint, s []netstat.SockTabEntry) map[string]uint {
 	connection_counts_new := make(map[string]uint)
 	lookup := func(skaddr *netstat.SockAddr) (string,uint) {
@@ -60,7 +45,7 @@ func countSockInfo(connection_counts map[string]uint, s []netstat.SockTabEntry) 
 	for _, e := range s {
 		saddr,sport := lookup(e.LocalAddr)
 		daddr,dport := lookup(e.RemoteAddr)
-		cur_c := string(daddr+":"+strconv.Itoa(int(dport)) + "|" + saddr + strconv.Itoa(int(sport)))
+		cur_c := string(daddr+"_"+strconv.Itoa(int(dport)) + "|" + saddr +"_"+strconv.Itoa(int(sport)))
 		//fmt.Printf("%s %s %s \n", saddr, daddr, e.State)
 		if((port==-1||port==int(sport))&&sport!=myport) {
 			connection_counts_new[cur_c] = connection_counts[cur_c] + 1
@@ -69,8 +54,11 @@ func countSockInfo(connection_counts map[string]uint, s []netstat.SockTabEntry) 
 	return(connection_counts_new)
 }
 func main() {
+//get flags
 	flag.BoolVar(&tcpv6, "tcpv6", true, "Should TCPV6 sockets be monitored?")
 	flag.IntVar(&port, "port", -1, "The port that should be monitored. -1 monitors every port.")
+	flag.IntVar(&myport, "listen", 9669, "The port on that this exporter listens for requests.")
+	flag.IntVar(&duration, "duration", 6, "The minimal duration in seconds after a connection is concerned as longterm.")
 	flag.Parse()
 	connections := make(map[string]uint)
 	connections6 := make(map[string]uint)
@@ -98,25 +86,38 @@ func main() {
 			time.Sleep(1 * time.Second)
 		}
 	}()
-	go func() {
-		for{
-			var sum uint = 0
-			for _, value := range(connections) {
-				if(value > 6) {
-					sum += 1
-				}
-			}
-			if(tcpv6) {
-				for _, value := range(connections6) {
-					if(value > 6) {
+
+	go func() {//create a thread that counts and exports the metric
+		if(port==-1) {
+			for{
+				var sum uint = 0
+				for _, value := range(connections) {
+					if(value >= duration) {
 						sum += 1
 					}
 				}
+				if(tcpv6) {
+					for _, value := range(connections6) {
+						if(value >= duration) {
+							sum += 1
+						}
+					}
+				}
+				time.Sleep(1 * time.Second)
+				netstat_tcp_connection_longterm_counts.Set(float64(sum))
 			}
-			time.Sleep(1 * time.Second)
-			netstat_tcp_connection_longterm_counts.Set(float64(sum))
+		}	else{
+			for{
+				connection_counts_port := make(map[string]uint)
+				for connection, value :=range(connections){
+					var dport string = strings.SplitN(strings.SplitN(connection, "|", 2)[1], "_", 2)[2]
+					netstat_tcp_connection_longterm_counts.WithLabelValues(dport).Observe(value)
+				}
+				//Add tcpv6 here!
+			}
 		}
 	}()
 	http.Handle("/metrics", promhttp.Handler())
-    http.ListenAndServe(":"+strconv.Itoa(myport) , nil)
+  http.ListenAndServe(":"+strconv.Itoa(myport) , nil)
+	//Add welcome page here
 }
